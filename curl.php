fca47f3c04bdb70c2981879fa86f61d009e8564d
Stuart Yamartino <stuart@yamartino.com>
2014-05-31 18:18:37 -0400

    Initial Commit from Backup

---
+++ curl.php
@@ -0,0 +1,56 @@
+<?php
+//Created by Stuart Yamartino on 10/31/12
+$db = new mysqli('localhost', 'root', 'YEGzxtKvWYTyLP3M', 'dsontweets');
+if(mysqli_connect_errno())
+{
+	echo "Connection Failed: " . mysqli_connect_errno();
+	exit();
+}
+$stmt = $db->stmt_init();
+
+require_once('usernames.php');
+
+
+for($a=1;$a<=count($usernames);$a++)
+{
+	$username = $usernames[$a];
+	$stmt->prepare("SELECT MAX(timestamp) FROM tweets WHERE username = ?");
+	$stmt->bind_param('s', $username);
+	$stmt->execute();
+	$stmt->bind_result($timestamp);
+	while($stmt->fetch()) 
+	{
+		$timestamp = $timestamp;
+	}
+	
+	if(is_null($timestamp))
+	{
+		$timestamp = 0;
+	}
+	
+	$content = file_get_contents("http://api.twitter.com/1/statuses/user_timeline.json?screen_name=$username&count=20"); 
+	$json = json_decode($content, TRUE);
+	
+	$i = 0;
+	$newstamp = strtotime($json[$i]['created_at']);
+	
+	if($newstamp > strtotime('Nov 06 00:00:00 +0000 2012') && $newstamp < strtotime('Feb 30 00:00:00 +0000 2013'))
+	{
+		while($timestamp < $newstamp && $newstamp > strtotime('Nov 06 00:00:00 +0000 2012') && $newstamp < strtotime('Feb 30 00:00:00 +0000 2013'))
+		{
+			$i++;
+			$newstamp = strtotime($json[$i]['created_at']);
+		}
+		
+		for($j=$i-1; $j>=0; $j--)
+		{
+			$datetime = date_create_from_format('D M d H:i:s O Y', $json[$j]['created_at']);
+			$date = date_format($datetime, 'm-d-y');
+			$time = date_format($datetime, 'H:i');
+			$stmt->prepare("INSERT INTO tweets (username, text, date, time, retweets, followers, timestamp) VALUES (?, ?, ?, ?, ?, ?, ?)");
+			$stmt->bind_param('ssssiii', $username, $json[$j]['text'], $date, $time, $json[$j]['retweet_count'], $json[$j]['user']['followers_count'],strtotime($json[$j]['created_at']));
+			$stmt->execute();
+		}
+	}
+}
+?>
---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+	$dbHost	= 'localhost';			//	database host
+	$dbUser	= 'root';		//	database user
+	$dbPass	= 'YEGzxtKvWYTyLP3M';		//	database password
+	$dbName	= 'dsontweets'; 		//	database name
+	$dbTable = 'tweets';
+
+	$connection = @mysql_connect($dbHost, $dbUser, $dbPass) or die("Couldn't connect.");
+	$db = mysql_select_db($dbName, $connection) or die("Couldn't select database.");
+
+	/*
+mysql_query("update $dbTable set text=replace(text,'â€™','\'');");
+	mysql_query("update $dbTable set text=replace(text,'â€¦','...');");
+	mysql_query("update $dbTable set text=replace(text,'â€“','-');");
+	mysql_query("update $dbTable set text=replace(text,'â€œ','\"');");
+	mysql_query("update $dbTable set text=replace(text,'â€','\"');");
+	mysql_query("update $dbTable set text=replace(text,'â€˜','\'');");
+	mysql_query("update $dbTable set text=replace(text,'â€¢','-');");
+	mysql_query("update $dbTable set text=replace(text,'â€¡','c');");
+*/
+	
+	
+	$sql = "SELECT tweetskey, username, text, date, time, retweets, followers FROM $dbTable";
+	$result = @mysql_query($sql)	or die("Couldn't execute query:".mysql_error().''.mysql_errno());
+	
+	
+	header('Content-Type: application/vnd.ms-excel');	//define header info for browser
+	header('Content-Disposition: attachment; filename='.$dbTable.'-'.date('d-m-Y').'.xls');
+	header('Pragma: no-cache');
+	header('Expires: 0');
+
+	for ($i = 0; $i < mysql_num_fields($result); $i++)	 // show column names as names of MySQL fields
+		echo mysql_field_name($result, $i)."\t";
+	print("\n");
+
+	while($row = mysql_fetch_row($result))
+	{
+		//set_time_limit(60); // you can enable this if you have lot of data
+		$output = '';
+		for($j=0; $j < mysql_num_fields($result); $j++)
+		{
+			if(!isset($row[$j]))
+			{
+				$output .= "NULL\t";
+			}
+			else 
+			{
+				$temp = $row[$j];
+				$t = str_replace('â€™','\'',str_replace('â€¦','...',str_replace('â€“','-',str_replace('â€œ','"',str_replace('â€','"',str_replace('â€˜','\'',str_replace('â€¢','-',str_replace('â€¡','c',$temp))))))));
+				$output .= "$t\t";
+			}
+		}
+		$output = preg_replace("/\r\n|\n\r|\n|\r/", ' ', $output);
+		print(trim($output))."\t\n";
+	}
+?>
